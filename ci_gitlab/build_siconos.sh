5d5f630fe2bbcce114f3c4ee7f09d171004e580a
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-11-06 10:46:59 +0100

    Update ci_gitlab/build_siconos.sh, ci_gitlab/configure_siconos.sh files

--- ci_gitlab/build_siconos.sh
+++ ci_gitlab/build_siconos.sh
@@ -30,7 +30,8 @@
 # - USER_FILE : user options file.
 # - OSNAME : set to IMAGE_NAME
 # - ALLOW_PARALLEL_BUILD : set to 1 to allow -jN, 0 to restrict to -j1.
-ctest -S ${CI_PROJECT_DIR}/ci_gitlab/ctest_driver_install_siconos.cmake -Dmodel=$ctest_build_model -DSICONOS_INSTALL_DIR=${CI_PROJECT_DIR}/install-siconos -DUSER_FILE=$1 -DOSNAME=$IMAGE_NAME -DALLOW_PARALLEL_BUILD=$allow_parallel_build -DCDASH_SUBMIT=0 -V -DJOB_MODE=1
+ls -altr
+ctest -S ${CI_PROJECT_DIR}/ci_gitlab/ctest_driver_install_siconos.cmake -Dmodel=$ctest_build_model -DSICONOS_INSTALL_DIR=${CI_PROJECT_DIR}/install-siconos -DUSER_FILE=$1 -DOSNAME=$IMAGE_NAME -DALLOW_PARALLEL_BUILD=$allow_parallel_build -DCDASH_SUBMIT=1 -VV -DJOB_MODE=1
 #cmake $CI_PROJECT_DIR
 #make -j 4
 
