66e46d5853480db8d04475cbe4736348aa0a3d50
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-11-09 17:45:39 +0100

    tests

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,9 +10,9 @@ set(CTEST_PROJECT_NAME "Siconos")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
 # https is needed on cdash server side
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "my.cdash.org/")
-set(CTEST_DROP_LOCATION "/submit.php?project=Siconos+Sandbox")
+#set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "https://my.cdash.org//submit.php?project=sandbox")
+#set(CTEST_DROP_LOCATION "/submit.php?project=Siconos+Sandbox")
 set(CTEST_DROP_SITE_CDASH TRUE)
 if(BUILD_NAME)
   set(CTEST_BUILD_NAME Siconos-${BUILD_NAME})
