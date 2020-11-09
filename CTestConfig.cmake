4d6411bcd2dd82eba0d248bcb5e962eb68c2827a
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-11-09 14:43:19 +0100

    Update ci_gitlab/gitlab-ci-siconos-templates.yml, CTestConfig.cmake files

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,7 +10,7 @@ set(CTEST_PROJECT_NAME "Siconos")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
 # https is needed on cdash server side
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org/")
 set(CTEST_DROP_LOCATION "/submit.php?project=Siconos+Sandbox")
 set(CTEST_DROP_SITE_CDASH TRUE)
