cac64e1528161b53a972560507a0d28fa0ae8b15
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-11-24 18:35:48 +0100

    tests ...

--- ci_gitlab/ctest_common.cmake
+++ ci_gitlab/ctest_common.cmake
@@ -1,216 +1,216 @@
-#----------------------------------------------------------------
-# Ctest driver : command lines common to all
-# ctest drivers.
-#
-# Usage, add at the end of your driver file :
-# include(<path-to>/ctest_common.cmake)
-# See for instance ctest_driver_install_siconos.cmake
-# ---------------------------------------------------------------
+#[=======================================================================[.rst:
+ctest_common.cmake
+----
+This file contains:
 
-# --- Configure setup ---
+- some functions used to setup ctest config
+- the ctest configuration (global variables like CTEST_SOURCE_DIR and so on)
+
+#]=======================================================================]
+
+#[=======================================================================[.rst:
+Call this function after each ctest step (ctest_configure,ctest_build ...)
+to handle errors and submission to cdash
+
+Usage :
+
+post_ctest(PHASE <phase_name>)
+
+with phase_name in (Configure, Build, Test).
+#]=======================================================================]
+function(post_ctest)
+  set(oneValueArgs PHASE)
+  cmake_parse_arguments(run "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  message("------> status/result : ${_STATUS}/${_RESULT}")
+  if(${run_PHASE} STREQUAL Configure)
+    set(parts ${run_PHASE} Notes Done)
+  else()
+    set(parts ${run_PHASE} Done)
+
+  endif()
+  if(CDASH_SUBMIT)
+    ctest_submit(
+      PARTS ${parts}
+      RETURN_VALUE RETURN_STATUS
+      CAPTURE_CMAKE_ERROR SUBMISSION_STATUS
+      )
+  else()
+    message("- Results won't be submitted to a cdash server.\n")
+    return()
+  endif()
+
+  if(NOT _STATUS EQUAL 0 OR NOT _RESULT EQUAL 0)
+    message(FATAL_ERROR "\n\n *** ${run_PHASE} process failed *** \n\n")
+  endif()
+  unset(_RESULT PARENT_SCOPE)
+  unset(_STATUS PARENT_SCOPE)
+  message("=============== End of ctest ${run_PHASE} =============== ")
+  if(NOT SUBMISSION_STATUS EQUAL 0)
+    message(WARNING " *** submission failure *** ")
+  endif()
+
+endfunction()
+
+# Set site name (cdash) according to current host status. 
+function(set_site_name)
+  
+  # -- Query host system information --
+  # --> to set ctest site for cdash.
+  #include(cmake_host_system_information)
+  cmake_host_system_information(RESULT hostname QUERY HOSTNAME)
+  cmake_host_system_information(RESULT fqdn QUERY FQDN)
+
+  if(${CMAKE_VERSION} VERSION_GREATER "3.10.3") 
+    cmake_host_system_information(RESULT osname QUERY OS_NAME)
+    cmake_host_system_information(RESULT osrelease QUERY OS_RELEASE)
+    cmake_host_system_information(RESULT osversion QUERY OS_VERSION)
+    cmake_host_system_information(RESULT osplatform QUERY OS_PLATFORM)
+  else()
+    set(osname ${CMAKE_SYSTEM_NAME})
+    set(osversion ${CMAKE_SYSTEM_VERSION})
+    set(osplatform ${CMAKE_SYSTEM_PROCESSOR})
+  endif()
+
+  # With gitlab-ci, runner name is too long and useless ...
+  string(FIND ${hostname} "runner-" on_ci) 
+  if(on_ci GREATER -1)
+    set(hostname "runner: $ENV{CI_RUNNER_DESCRIPTION}")
+  endif()
+
+  # Host description
+  if(NOT OSNAME)
+    set(OSNAME ${osname}) # Use -DOSNAME=docker_image name on CI
+  endif()
+  
+  string(REPLACE
+    "gricad-registry.univ-grenoble-alpes.fr/nonsmooth/siconos/"
+    "registry, "
+    OSNAME ${OSNAME})
+  
+  set(CTEST_SITE "${OSNAME} ${osrelease}, ${osplatform}, ${hostname}" PARENT_SCOPE)
+
+endfunction()
+
+# set build name, according to host, ci, git status ...
+function(set_cdash_build_name)
+  # Get hash for commit of current version of Siconos
+  # Saved by CI in CI_COMMIT_SHORT_SHA.
+  if($ENV{GITLAB_CI})
+    set(branch_commit "$ENV{CI_COMMIT_REF_NAME}/$ENV{CI_COMMIT_SHORT_SHA}")
+  elseif($ENV{TRAVIS})
+    set(branch_commit "$ENV{TRAVIS_BRANCH}/$ENV{TRAVIS_COMMIT}")
+  else()
+    find_package(Git)
+    execute_process(COMMAND
+      ${GIT_EXECUTABLE} rev-parse --short HEAD
+      OUTPUT_VARIABLE COMMIT_SHORT_SHA
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      WORKING_DIRECTORY $ENV{CI_PROJECT_DIR})
+    execute_process(COMMAND
+      ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+      OUTPUT_VARIABLE COMMIT_REF_NAME
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      WORKING_DIRECTORY $ENV{CI_PROJECT_DIR})
+    set(branch_commit "${COMMIT_REF_NAME}/${COMMIT_SHORT_SHA}")
+  endif()
+  include(${CTEST_SOURCE_DIRECTORY}/cmake/SiconosVersion.cmake)  
+  set(CTEST_BUILD_NAME "Siconos (${SICONOS_VERSION}-devel, branch/commit=${branch_commit}")
+  if(USER_FILE)
+    get_filename_component(_name ${USER_FILE} NAME)
+    set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME} - Option file : ${_name}")
+  endif()
+
+  set(CTEST_BUILD_NAME ${CTEST_BUILD_NAME} PARENT_SCOPE)
+  
+endfunction()
+
+# ------------------
+# Here starts ctest config
+# ------------------
+
+if($ENV{TRAVIS})
+
+  list(APPEND CMAKE_MODULE_PATH ${CTEST_SOURCE_DIRECTORY}/ci_travis/cmake)
+  list(APPEND CMAKE_MODULE_PATH ${CTEST_SOURCE_DIRECTORY}/ci_travis/config)
+  list(APPEND CMAKE_MODULE_PATH ${CTEST_SOURCE_DIRECTORY}/ci_travis)
+  include(Tools)
+
+  # -- Get config --
+  # i.e. set extra options/values (cmake -Doption=value ...)
+  # either from file default.cmake or
+  # from file CI_CONFIG.cmake
+  # --> may set SICONOS_CMAKE_OPTIONS
+  # --> may set DSICONOS_COMPONENTS
+  # Rq : For Travis CI, we include cmake files while for gitlab CI we use
+  # siconos user option file. Todo: one way to rule them all?
+
+  if(CI_CONFIG)
+    string(REPLACE "," ";" CI_CONFIG_LIST ${CI_CONFIG})
+    foreach(_CI IN LISTS CI_CONFIG_LIST)
+      include(${_CI})
+    endforeach()
+  else()
+    set(CI_CONFIG default)
+    include(${CI_CONFIG})
+  endif()
+endif()
+
+# - Source dir and path to siconos install
+if(NOT CTEST_SOURCE_DIRECTORY)
+  set(CTEST_SOURCE_DIRECTORY $ENV{CI_PROJECT_DIR})
+endif()
+
 # - Top level build directory -
 # If not specified : current dir.
 if(NOT CTEST_BINARY_DIRECTORY)
   set(CTEST_BINARY_DIRECTORY .)
 endif()
 
-# Current testing model. Priority: 
-# Nightly -> set by scheduler on gricad-gitlab
-# Continuous -> set in .gitlab-ci.yml
-# Experimental : default
-if(NOT model)
-  set(model Experimental)
+# Install dir (used as CMAKE_INSTALL_PREFIX)
+# Must be in ${CI_PROJECT_DIR} to work properly with ci artifacts and similar.
+if(NOT SICONOS_INSTALL_DIR)
+  set(SICONOS_INSTALL_DIR $ENV{CI_PROJECT_DIR}/install-siconos/)
 endif()
+
+# Build name (for cdash)
+if(NOT CTEST_BUILD_NAME)
+  set_cdash_build_name()
+endif()
+
+if(USER_FILE)
+  list(APPEND SICONOS_CMAKE_OPTIONS -DUSER_OPTIONS_FILE=${USER_FILE})
+endif()
+
+list(APPEND SICONOS_CMAKE_OPTIONS -DCMAKE_INSTALL_PREFIX=${SICONOS_INSTALL_DIR})
+
+if(DEFINED ENV{OCE_INSTALL}) # set if oce has been installed using oce repo, in install_oce.sh
+  message("Search oce in $ENV{OCE_INSTALL}.")
+  list(APPEND SICONOS_CMAKE_OPTIONS -DOCE_DIR=$ENV{OCE_INSTALL})
+endif()
+
+# Parallel build only for siconos_install. For examples it leads to: warning: jobserver unavailable: using -j1. Add `+' to parent make rule.
+#set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CTEST_SOURCE_DIRECTORY}/cmake/valgrind.supp)
 
 if(NOT CTEST_CMAKE_GENERATOR)
   set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 endif()
 
-# -- Query host system information --
-# --> to set ctest site for cdash.
-#include(cmake_host_system_information)
-cmake_host_system_information(RESULT hostname QUERY HOSTNAME)
-cmake_host_system_information(RESULT fqdn QUERY FQDN)
-cmake_host_system_information(RESULT NP QUERY NUMBER_OF_LOGICAL_CORES)
-
 # if(NOT DEFINED ALLOW_PARALLEL_BUILD)
 #   set(ALLOW_PARALLEL_BUILD ON)
 # endif()
 # option(ALLOW_PARALLEL_BUILD "Allow parallel build" ${ALLOW_PARALLEL_BUILD})
+cmake_host_system_information(RESULT NP QUERY NUMBER_OF_LOGICAL_CORES)
 if(NOT ALLOW_PARALLEL_BUILD)
   set(NP 1)
 endif()
 set(CTEST_BUILD_FLAGS -j${NP})
 
-if(${CMAKE_VERSION} VERSION_GREATER "3.10.3") 
-  cmake_host_system_information(RESULT osname QUERY OS_NAME)
-  cmake_host_system_information(RESULT osrelease QUERY OS_RELEASE)
-  cmake_host_system_information(RESULT osversion QUERY OS_VERSION)
-  cmake_host_system_information(RESULT osplatform QUERY OS_PLATFORM)
-else()
-  set(osname ${CMAKE_SYSTEM_NAME})
-  set(osversion ${CMAKE_SYSTEM_VERSION})
-  set(osplatform ${CMAKE_SYSTEM_PROCESSOR})
-endif()
-
-# With gitlab-ci, runner name is too long and useless ...
-string(FIND ${hostname} "runner-" on_ci) 
-if(on_ci GREATER -1)
-  set(hostname "runner: $ENV{CI_RUNNER_DESCRIPTION}")
-endif()
-
-# Host description
-if(NOT OSNAME)
-  set(OSNAME ${osname}) # Use -DOSNAME=docker_image name on CI
-endif()
-
-string(REPLACE
-  "gricad-registry.univ-grenoble-alpes.fr/nonsmooth/siconos/"
-  "registry, "
-  OSNAME ${OSNAME})
-
 if(NOT CTEST_SITE)
-  set(CTEST_SITE "${OSNAME} ${osrelease}, ${osplatform}, ${hostname}")
+  set_site_name()
 endif()
 
-if(${CTEST_MODE} STREQUAL "configure" OR ${CTEST_MODE} STREQUAL "all")
-
-  ctest_start(${model})
-
-  # Set CTEST_CONFIGURE_COMMAND to cmake followed by siconos options 
-  set(CTEST_CONFIGURE_COMMAND ${CMAKE_COMMAND})
-  foreach(option IN LISTS SICONOS_CMAKE_OPTIONS)
-    set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} ${option}")
-  endforeach()
-  set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} ${CTEST_SOURCE_DIRECTORY}")
-
-  message("\n\n=============== Start ctest_configure =============== ")
-  message("- Configure command line :\n ${CTEST_CONFIGURE_COMMAND}\n")
-
-  ctest_configure(
-    RETURN_VALUE CONFIGURE_RESULT
-    CAPTURE_CMAKE_ERROR CONFIGURE_STATUS
-    #QUIET
-    )
-
-  message("=============== End of ctest_configure =============== ")
-  message("------> Configure status/result : ${CONFIGURE_STATUS}/${CONFIGURE_RESULT}")
-  if(NOT CONFIGURE_STATUS EQUAL 0 OR NOT CONFIGURE_RESULT EQUAL 0)
-    message(FATAL_ERROR "\n\n *** Configure (cmake) process failed *** \n\n")
-  endif()
+if(NOT CTEST_BUILD_CONFIGURATION)
+  set(CTEST_BUILD_CONFIGURATION "Release")
 endif()
 
-if(${CTEST_MODE} STREQUAL "build" OR ${CTEST_MODE} STREQUAL "all")
-
-  if(${CTEST_MODE} STREQUAL "build")
-    ctest_start(APPEND) # Restart from existing (configure step) cdash config
-  endif()
-  # --- Build ---
-
-  if(NOT CTEST_BUILD_CONFIGURATION)
-    set(CTEST_BUILD_CONFIGURATION "Release")
-  endif()
-
-  message("\n\n=============== Start ctest_build =============== ")
-
-  ctest_build(
-    PROJECT_NAME ${current_project}
-    CAPTURE_CMAKE_ERROR BUILD_STATUS
-    RETURN_VALUE BUILD_RESULT
-    #QUIET if quiet, travis failed because of missing outputs during a long time ...
-    )
-  message("=============== End of ctest_build =============== ")
-  message("------> Build status/result : ${BUILD_STATUS}/${BUILD_RESULT}")
-  if(NOT BUILD_STATUS EQUAL 0 OR NOT BUILD_RESULT EQUAL 0)
-    message(FATAL_ERROR " *** Build (make) process failed *** ")
-  endif()
-endif()
-
-if(${CTEST_MODE} STREQUAL "tests" OR ${CTEST_MODE} STREQUAL "all")
-  # -- Tests --
-  
-  if(${CTEST_MODE} STREQUAL "tests")
-    ctest_start(APPEND)
-  endif()
-  message("\n\n=============== Start ctest_test (nbprocs = ${NP}) =============== ")
-  ctest_test(
-    #PARALLEL_LEVEL NP
-    CAPTURE_CMAKE_ERROR TEST_STATUS
-    #SCHEDULE_RANDOM ON
-    RETURN_VALUE TEST_RESULT
-    # QUIET
-    )
-  message("=============== End of ctest_test =============== ")
-  message("------> Test status/result : ${TEST_STATUS}/${TEST_RESULT}")
-
-  if(WITH_MEMCHECK AND CTEST_COVERAGE_COMMAND)
-    #find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
-    # set(CTEST_COVERAGE_COMMAND gcov)
-
-    ctest_coverage(
-      CAPTURE_CMAKE_ERROR COVERAGE_STATUS
-      RETURN_VALUE COVERAGE_RESULT  
-      )
-  endif()
-
-  if(WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
-    ctest_memcheck()
-  endif()
-
-  # error status check later, we try to submit even if tests failed.
-
-  # -- memory check -- Skip this to 'enlight' submit process, since cdash inria is overbooked ...
-  # if(CTEST_BUILD_CONFIGURATION MATCHES "Profiling")
-  #   find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
-  #   set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--quiet --leak-check=full --show-leak-kinds=definite,possible --track-origins=yes --error-limit=no --gen-suppressions=all") 
-  #   set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--quiet --leak-check=full --show-reachable=yes --error-limit=no --gen-suppressions=all") 
-  #   ctest_memcheck(PARALLEL_LEVEL NP QUIET)
-  # endif()
-  message("=============== End of ctest_test =============== ")
-
-endif()
-
-if(CDASH_SUBMIT)
-  message("\n\n=============== Start ctest_submit =============== ")
-  message("- Results will be submitted to cdash server, ${CTEST_DROP_SITE}")
-  # -- Submission to cdash --
-  # file(GLOB SUBMIT_FILES ${CTEST_BINARY_DIRECTORY}/Testing/*/*)
-  # message(STATUS "submit files : ${SUBMIT_FILES}")
-  # message(STATUS "PATH : ${CTEST_BINARY_DIRECTORY}")
-  
-  ctest_submit(
-    # PARTS 
-    # FILES ${SUBMIT_FILES}
-    # SUBMIT_URL "http://my.cdash.org/submit.php?project=sandbox"
-    # Implemented in cmake 3.15
-    # BUILD_ID buildid # Implemented in cmake 3.15
-    RETURN_VALUE RETURN_STATUS
-    CAPTURE_CMAKE_ERROR SUBMISSION_STATUS
-    )
-else()
-  message("- Results won't be submitted to a cdash server.\n")
-  return()
-endif()
-
-
-# ============= Summary =============
-message(STATUS "\n============================================ Summary ============================================")
-message(STATUS "CTest process for ${current_project} has ended.")
-message(STATUS "Ctest model is: ${model}")
-message(STATUS "Ctest executed on sources directory : ${CTEST_SOURCE_DIRECTORY}")
-if(CDASH_SUBMIT)
-  message(STATUS "Build name (cdash) : ${CTEST_BUILD_NAME}")
-  message(STATUS "Site (cdash) : ${CTEST_SITE}")
-endif()
-message(STATUS "=================================================================================================\n")
-
-if(CTEST_MODE STREQUAL "tests")
-  # tests failed?
-  if(NOT TEST_STATUS EQUAL 0 OR NOT TEST_RESULT EQUAL 0)
-    message(FATAL_ERROR " *** test failure *** ")
-  endif()
-endif()
-# -- Submission failed? --
-if(NOT SUBMISSION_STATUS EQUAL 0)
-  message(WARNING " *** submission failure *** ")
-endif()
--- ci_gitlab/ctest_driver_install_siconos.cmake
+++ ci_gitlab/ctest_driver_install_siconos.cmake
@@ -11,44 +11,133 @@
 #   using common commands (lsb_release ...)
 # ----------------------------------------------
 
-# used as CMAKE_INSTALL_PREFIX
-if(NOT SICONOS_INSTALL_DIR)
-  set(SICONOS_INSTALL_DIR ../install-siconos/)
+
+message("--- Start conf for siconos ctest pipeline.")
+
+# ============= setup  ================
+
+# -- CI_PROJECT_DIR is a required environment variable --
+
+if($ENV{TRAVIS})
+  set(ENV{CI_PROJECT_DIR} $ENV{TRAVIS_BUILD_DIR})
 endif()
 
-# -- job : build and install siconos --
-message("--- Start conf for siconos install.")
-# - Source dir and path to siconos install
-if(NOT CTEST_SOURCE_DIRECTORY)
-  set(CTEST_SOURCE_DIRECTORY $ENV{CI_PROJECT_DIR})
+if(NOT DEFINED ENV{CI_PROJECT_DIR})
+  message(FATAL_ERROR "Please set env variable CI_PROJECT_DIR to siconos sources directory (git repo).")
 endif()
 
-# Build name (for cdash)
-if(NOT CTEST_BUILD_NAME)
-  # Get hash for commit of current version of Siconos
-  # Saved by CI in CI_COMMIT_SHORT_SHA.
-  include(${CTEST_SOURCE_DIRECTORY}/cmake/SiconosVersion.cmake)
-  set(CTEST_BUILD_NAME "Siconos (${SICONOS_VERSION}-devel, branch/commit=$ENV{CI_COMMIT_REF_NAME}/$ENV{CI_COMMIT_SHORT_SHA})")
-  if(USER_FILE)
-    get_filename_component(_name ${USER_FILE} NAME)
-    set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME} - Option file : ${_name}")
-  endif()    
-endif()
-
-
-if(USER_FILE)
-  set(SICONOS_CMAKE_OPTIONS -DUSER_OPTIONS_FILE=${USER_FILE})
-endif()
-
-list(APPEND SICONOS_CMAKE_OPTIONS -DCMAKE_INSTALL_PREFIX=${SICONOS_INSTALL_DIR})
-
-if(DEFINED ENV{OCE_INSTALL}) # set if oce has been installed using oce repo, in install_oce.sh
-  message("Search oce in $ENV{OCE_INSTALL}.")
-  list(APPEND SICONOS_CMAKE_OPTIONS -DOCE_DIR=$ENV{OCE_INSTALL})
-endif()
-
-set(current_project siconos_install)
-# Parallel build only for siconos_install. For examples it leads to: warning: jobserver unavailable: using -j1. Add `+' to parent make rule.
-#set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CTEST_SOURCE_DIRECTORY}/cmake/valgrind.supp)
-
+# -- Definition of all variables required for ctest --
 include($ENV{CI_PROJECT_DIR}/ci_gitlab/ctest_common.cmake)
+
+# =============  Run ctest steps ================
+# Either one by one (to split ci jobs) if CTEST_MODE=Configure, Build, Test or
+# all in a row if CTEST_MODE=all.
+
+# - Configure -- 
+if(${CTEST_MODE} STREQUAL "Configure" OR ${CTEST_MODE} STREQUAL "all")
+
+ # Current testing model. Priority: 
+# Nightly -> set by scheduler on gricad-gitlab
+# Continuous -> set in .gitlab-ci.yml
+# Experimental : default
+if(NOT model)
+  set(model Experimental)
+endif()
+
+  ctest_start(${model})
+
+  # Set CTEST_CONFIGURE_COMMAND to cmake followed by siconos options 
+  set(CTEST_CONFIGURE_COMMAND ${CMAKE_COMMAND})
+  foreach(option IN LISTS SICONOS_CMAKE_OPTIONS)
+    set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} ${option}")
+  endforeach()
+  set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} ${CTEST_SOURCE_DIRECTORY}")
+
+  message("\n\n=============== Start ctest_configure =============== ")
+  message("- Configure command line :\n ${CTEST_CONFIGURE_COMMAND}\n")
+
+  if(USER_FILE)
+    # Push user file as notes to cdash.
+    set(CTEST_NOTES_FILES ${USER_FILE})
+  endif()
+  ctest_configure(
+    RETURN_VALUE _RESULT
+    CAPTURE_CMAKE_ERROR _STATUS
+    #QUIET
+    )
+  post_ctest(PHASE Configure)
+endif()
+ 
+# - Build -
+if(${CTEST_MODE} STREQUAL "Build" OR ${CTEST_MODE} STREQUAL "all")
+
+  if(${CTEST_MODE} STREQUAL "Build")
+    ctest_start(APPEND) # Restart from existing (configure step) cdash config
+  endif()
+  # --- Build ---
+
+  message("\n\n=============== Start ctest_build =============== ")
+
+  ctest_build(
+    CAPTURE_CMAKE_ERROR _STATUS
+    RETURN_VALUE _RESULT
+    #QUIET if quiet, travis failed because of missing outputs during a long time ...
+    )
+  post_ctest(PHASE Build)
+endif()
+
+# - Test -
+if(${CTEST_MODE} STREQUAL "Test" OR ${CTEST_MODE} STREQUAL "all")
+  # -- Tests --
+  
+  if(${CTEST_MODE} STREQUAL "Test")
+    ctest_start(APPEND)
+  endif()
+  message("\n\n=============== Start ctest_test (nbprocs = ${NP}) =============== ")
+  ctest_test(
+    #PARALLEL_LEVEL NP
+    CAPTURE_CMAKE_ERROR _STATUS
+    #SCHEDULE_RANDOM ON
+    RETURN_VALUE _RESULT
+    # QUIET
+    )
+  post_ctest(PHASE Test)
+
+  if(WITH_MEMCHECK AND CTEST_COVERAGE_COMMAND)
+    #find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+    # set(CTEST_COVERAGE_COMMAND gcov)
+
+    ctest_coverage(
+      CAPTURE_CMAKE_ERROR COVERAGE_STATUS
+      RETURN_VALUE COVERAGE_RESULT  
+      )
+  endif()
+
+  if(WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+    ctest_memcheck()
+  endif()
+
+  # error status check later, we try to submit even if tests failed.
+
+  # -- memory check -- Skip this to 'enlight' submit process, since cdash inria is overbooked ...
+  # if(CTEST_BUILD_CONFIGURATION MATCHES "Profiling")
+  #   find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+  #   set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--quiet --leak-check=full --show-leak-kinds=definite,possible --track-origins=yes --error-limit=no --gen-suppressions=all") 
+  #   set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--quiet --leak-check=full --show-reachable=yes --error-limit=no --gen-suppressions=all") 
+  #   ctest_memcheck(PARALLEL_LEVEL NP QUIET)
+  # endif()
+
+endif()
+
+# ============= Summary =============
+message(STATUS "\n============================================ Summary ============================================")
+message(STATUS "CTest process for Siconos has ended.")
+message(STATUS "Ctest model is: ${model}")
+message(STATUS "Ctest executed on sources directory : ${CTEST_SOURCE_DIRECTORY}")
+message(STATUS "CTEST_BINARY_DIRECTORY is: ${CTEST_BINARY_DIRECTORY}")
+message(STATUS "CTEST_BUILD_CONFIGURATION is: ${CTEST_BUILD_CONFIGURATION}")
+if(CDASH_SUBMIT)
+  message(STATUS "Build name (cdash) : ${CTEST_BUILD_NAME}")
+  message(STATUS "Site (cdash) : ${CTEST_SITE}")
+endif()
+message(STATUS "=================================================================================================\n")
